b6dc8c6067377a0ff447963c5cb7794a3364f623
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-09 17:53:57 +0900

    git init

---
+++ src/main/java/org/ktb/modie/ModieApplication.java
@@ -0,0 +1,13 @@
+package org.ktb.modie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModieApplication.class, args);
+    }
+
+}
