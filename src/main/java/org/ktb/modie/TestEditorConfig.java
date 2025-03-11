89ab63b90713a006b3b2996291ac74f61350f09b
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-12 07:51:49 +0900

    feat: jacoco 설정 추가

---
+++ src/main/java/org/ktb/modie/TestEditorConfig.java
@@ -0,0 +1,15 @@
+package org.ktb.modie;
+
+public class TestEditorConfig {
+	// 들여쓰기 없음 (완전히 누락)
+	public static void main(String[] args) {
+		// 중첩된 들여쓰기도 없음
+		System.out.println("Hello");
+	}
+
+	// 줄 끝 공백 추가
+	public void test() {
+
+		System.out.println("공백");
+	}
+}
