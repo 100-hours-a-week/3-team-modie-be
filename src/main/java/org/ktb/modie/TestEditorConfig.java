1ba4694823a15f26efcb39dac1004761cf866a1d
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-13 01:08:02 +0900

    fix: semantic 테스트용 주석 추가

--- src/main/java/org/ktb/modie/TestEditorConfig.java
+++ src/main/java/org/ktb/modie/TestEditorConfig.java
@@ -2,7 +2,6 @@ package org.ktb.modie;
 
 public class TestEditorConfig {
     // 들여쓰기 없음 (완전히 누락)
-    // 버전 테스트트트트트트트트트트트트트트트트트트트트트트트트트트트
     public static void main(String[] args) {
 // 중첩된 들여쓰기도 없음
         System.out.println("Hello");
