97ec262f18858c23a4d8ff1730c77a9f0d5a44a2
YEACHAN OH <dus001228@naver.com>
2025-03-13 16:21:34 +0900

    test: git push 403 test

--- src/main/java/org/ktb/modie/TestEditorConfig.java
+++ src/main/java/org/ktb/modie/TestEditorConfig.java
@@ -1,9 +1,8 @@
 package org.ktb.modie;
 
 public class TestEditorConfig {
-    // 들여쓰기 없음 (완전히 누락)
     public static void main(String[] args) {
-        System.out.println("Hello"); // test-jade 배고파
+        System.out.println("Hello");
     }
 
     // 줄 끝 공백 추가
