0f83cd48a6a772691921c044279048ff4c5128ee
TAEHYEON <disney1026dlxogus@gmail.com>
2025-03-13 13:54:01 +0900

    test: pre-commit hook test

--- src/main/java/org/ktb/modie/TestEditorConfig.java
+++ src/main/java/org/ktb/modie/TestEditorConfig.java
@@ -3,8 +3,7 @@ package org.ktb.modie;
 public class TestEditorConfig {
     // 들여쓰기 없음 (완전히 누락)
     public static void main(String[] args) {
-// 중첩된 들여쓰기도 없음
-        System.out.println("Hello");
+        System.out.println("Hello"); // test-jade 배고파
     }
 
     // 줄 끝 공백 추가
