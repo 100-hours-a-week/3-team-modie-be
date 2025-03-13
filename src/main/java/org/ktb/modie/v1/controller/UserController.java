e6f5875560dadf9555ad0c683451833eb2064ce5
uhyunglee <uhyunglee@naver.com>
2025-03-13 23:14:15 +0900

    feat(user): update account number swagger api

--- src/main/java/org/ktb/modie/v1/controller/UserController.java
+++ src/main/java/org/ktb/modie/v1/controller/UserController.java
@@ -4,12 +4,19 @@ import org.ktb.modie.core.response.SuccessResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController implements UserApi {
     @GetMapping("/api/v1/users")
     public ResponseEntity<SuccessResponse<Void>> getUserProfile() {
+
+        return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
+    }
+
+    @PatchMapping("/api/v1/users/accounts")
+    public ResponseEntity<SuccessResponse<Void>> updateUserAccounts(String bankName, String accountNumber) {
 
         return SuccessResponse.ofNoData().asHttp(HttpStatus.OK);
     }
