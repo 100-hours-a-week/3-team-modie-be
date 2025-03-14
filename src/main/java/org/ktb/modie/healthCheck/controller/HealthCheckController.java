5ca0a226856cbef15ca03f3e1d2463fb768eab68
TAEHYEON <disney1026dlxogus@gmail.com>
2025-03-14 13:09:10 +0900

    feat: Add health check endpoint

---
+++ src/main/java/org/ktb/modie/healthCheck/controller/HealthCheckController.java
@@ -0,0 +1,33 @@
+package org.ktb.modie.healthCheck.controller;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        try (Connection conn = dataSource.getConnection()) {
+            if (conn.isValid(2)) {
+                return ResponseEntity.ok("OK");
+            } else {
+                return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("DB Connection Failed");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("DB Connection Error");
+        }
+    }
+}
