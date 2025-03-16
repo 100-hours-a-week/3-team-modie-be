399f1857f472e3b6f861ec8c59088677d7a93ff9
uhyunglee <uhyunglee@naver.com>
2025-03-16 15:31:25 +0900

    feat(meet): update payments swagger api and check style final

--- src/main/java/org/ktb/modie/v1/controller/MeetApi.java
+++ src/main/java/org/ktb/modie/v1/controller/MeetApi.java
@@ -15,7 +15,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "모임 API", description = "모임 관련 API")
 @Validated
