99181ac6150bfad869b5431c4a8add89218c694a
JiwonHwang <cutty82432@gmail.com>
2025-03-17 17:52:22 +0900

    feat : make MeetListResponseDto, MeetSummaryDto for getMeetList api

---
+++ src/main/java/org/ktb/modie/v1/dto/MeetListResponseDto.java
@@ -0,0 +1,20 @@
+package org.ktb.modie.v1.dto;
+
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record MeetListResponseDto(
+    @Schema(description = "페이지 번호", example = "1")
+    int page,
+
+    @Schema(description = "페이지 크기", example = "10")
+    int size,
+
+    @Schema(description = "전체 요소 수", example = "47")
+    long totalElements,
+
+    @Schema(description = "모임 리스트")
+    List<MeetSummaryDto> meets
+) {
+}
