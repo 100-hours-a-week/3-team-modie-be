00c4593115cd84174645ebb3320524b55a75aa6c
uhyunglee <uhyunglee@naver.com>
2025-03-17 10:28:49 +0900

    fix: remove request body confliction

--- src/main/java/org/ktb/modie/v1/controller/MeetApi.java
+++ src/main/java/org/ktb/modie/v1/controller/MeetApi.java
@@ -2,26 +2,26 @@ package org.ktb.modie.v1.controller;
 
 import java.util.Map;
 
-import io.swagger.v3.oas.annotations.Parameter;
-import jakarta.validation.constraints.Digits;
 import org.ktb.modie.core.response.SuccessResponse;
 import org.ktb.modie.v1.dto.MeetDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Digits;
 
 @Tag(name = "모임 API", description = "모임 관련 API")
 @Validated
