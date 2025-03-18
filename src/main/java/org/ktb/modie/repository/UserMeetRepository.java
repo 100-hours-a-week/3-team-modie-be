3701fd2ae369f6eb5bd5e977f40febc0e3b96682
JiwonHwang <cutty82432@gmail.com>
2025-03-18 22:07:37 +0900

    feat : update UserMeetRepository codeStyle

--- src/main/java/org/ktb/modie/repository/UserMeetRepository.java
+++ src/main/java/org/ktb/modie/repository/UserMeetRepository.java
@@ -1,10 +1,8 @@
 package org.ktb.modie.repository;
 
-import org.ktb.modie.domain.Meet;
-import org.ktb.modie.domain.User;
 import org.ktb.modie.domain.UserMeet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserMeetRepository extends JpaRepository<UserMeet, Long> {
-    boolean existsByUserAndMeet(User user, Meet meet);
+    boolean isExistsByUserAndMeet(String userId, Long meetId);
 }
