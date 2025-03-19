3e98b682fd5c815f1bb69ba35d674d7039eb09ec
JiwonHwang <cutty82432@gmail.com>
2025-03-19 12:35:59 +0900

    feat : Update UserMeetRepository - Add count query

--- src/main/java/org/ktb/modie/repository/UserMeetRepository.java
+++ src/main/java/org/ktb/modie/repository/UserMeetRepository.java
@@ -4,9 +4,14 @@ import org.ktb.modie.domain.Meet;
 import org.ktb.modie.domain.User;
 import org.ktb.modie.domain.UserMeet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserMeetRepository extends JpaRepository<UserMeet, Long> {
     boolean existsByUserAndMeet(User user, Meet meet);
 
     int countByMeet(Meet meet);
+
+    @Query("SELECT COUNT(um) FROM UserMeet um WHERE um.meet.meetId = :meetId AND um.isPayed = false")
+    Long countUnpaidUsersByMeetId(@Param("meetId") Long meetId);
 }
