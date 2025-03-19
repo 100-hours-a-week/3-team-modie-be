bc0a190ec8a45f0371e220df7dc050b53454f875
JiwonHwang <cutty82432@gmail.com>
2025-03-19 17:49:56 +0900

    feat : Add PaymentUpdate Req&Res Dto #44

---
+++ src/main/java/org/ktb/modie/presentation/v1/dto/PaymentUpdateResponse.java
@@ -0,0 +1,12 @@
+package org.ktb.modie.presentation.v1.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PaymentUpdateResponse(
+    @Schema(description = "정산 상태 변경 대상 유저 ID", example = "123456")
+    String userId,
+
+    @Schema(description = "정산 완료 여부 (1 = 완료, 0 = 미완료)", example = "1")
+    boolean isPayed
+) {
+}
