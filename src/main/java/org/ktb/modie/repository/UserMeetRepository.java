69ed1033fe6c12d741a6964448a0c93151e3d979
JiwonHwang <cutty82432@gmail.com>
2025-03-19 16:34:13 +0900

    feat : Update UserMeetRepository - Update existsByUserAndMeet findUserMeetByUser_UserIdAndMeet_MeetId #37

--- src/main/java/org/ktb/modie/repository/UserMeetRepository.java
+++ src/main/java/org/ktb/modie/repository/UserMeetRepository.java
@@ -1,12 +1,18 @@
 package org.ktb.modie.repository;
 
+import java.util.Optional;
+
 import org.ktb.modie.domain.Meet;
-import org.ktb.modie.domain.User;
 import org.ktb.modie.domain.UserMeet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserMeetRepository extends JpaRepository<UserMeet, Long> {
-    boolean existsByUserAndMeet(User user, Meet meet);
+    Optional<UserMeet> findUserMeetByUser_UserIdAndMeet_MeetId(String userId, Long meetId);
 
     int countByMeet(Meet meet);
+
+    @Query("SELECT COUNT(um) FROM UserMeet um WHERE um.meet.meetId = :meetId AND um.isPayed = false")
+    Long countUnpaidUsersByMeetId(@Param("meetId") Long meetId);
 }
