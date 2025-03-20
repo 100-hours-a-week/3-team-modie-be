5e715e6466ac083ac7469feca26aeb1c9ac83c4e
TAEHYEON <disney1026dlxogus@gmail.com>
2025-03-20 23:02:54 +0900

    fix: cors 수정

--- src/main/java/org/ktb/modie/config/WebSocketConfig.java
+++ src/main/java/org/ktb/modie/config/WebSocketConfig.java
@@ -10,18 +10,19 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // SockJS와 함께 엔드포인트 설정
-        registry.addEndpoint("/ws")  // 클라이언트가 연결할 엔드포인트
-            .setAllowedOrigins("http://localhost:5173", "http://local:8080", "https://modie.site", "https://dev.modie.site", "http://dev.modie.site", "https://dev-api.modie.site", "http://dev-api.modie.site")
-            // .addInterceptors(new CustomHandshakeInterceptor())  // 사용자 정보 추가
-            .withSockJS(); // SockJS 지원
-    }
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		// SockJS와 함께 엔드포인트 설정
+		registry.addEndpoint("/ws")  // 클라이언트가 연결할 엔드포인트
+			// .setAllowedOrigins("http://localhost:5173", "ws://localhost:5173") // http와 ws 프로토콜을 모두 허용
+			.setAllowedOriginPatterns("*");
+		// .withSockJS(); // SockJS 지원
+	}
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic"); // 구독 채널
-        registry.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 보낼 때 사용하는 prefix
-    }
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.enableSimpleBroker("/topic"); // 구독 채널
+		registry.setApplicationDestinationPrefixes("/app"); // 클라이언트에서 보낼 때 사용하는 prefix
+	}
 }
+
