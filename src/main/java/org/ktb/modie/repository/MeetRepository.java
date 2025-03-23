17ab4c5cdf375c6c88d1b072a873e66fc4aa7565
sanghyuk Kim <tkdgur5031@naver.com>
2025-03-23 17:21:29 +0900

    fix: 빌드 오류 해결

--- src/main/java/org/ktb/modie/repository/MeetRepository.java
+++ src/main/java/org/ktb/modie/repository/MeetRepository.java
@@ -1,7 +1,5 @@
 package org.ktb.modie.repository;
 
-import java.util.Optional;
-
 import org.ktb.modie.domain.Meet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,10 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MeetRepository extends JpaRepository<Meet, Long> {
     Optional<Meet> findByMeetId(Long meetId);
 
-    // ✅ meetType과 isCompleted(완료시간 기준)를 기준으로 필터링 (+페이징 처리)
+    // ✅ meetType과 isCompleted(완료시간 기준)를 기준으로 필터링 (+페이징 처리) - 최신
     @Query("SELECT m FROM Meet m WHERE "
         + "(:meetType IS NULL OR m.meetType = :meetType) "
         + "AND ((:isCompleted = false AND m.completedAt IS NULL) "
