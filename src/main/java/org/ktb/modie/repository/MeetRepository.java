14945ec9e44afc85a83f312d7caf5622ebf1fa41
JiwonHwang <cutty82432@gmail.com>
2025-03-26 09:38:36 +0900

    fix : update MeetRepository - Update query on findFilteredMeets #89

--- src/main/java/org/ktb/modie/repository/MeetRepository.java
+++ src/main/java/org/ktb/modie/repository/MeetRepository.java
@@ -1,13 +1,13 @@
 package org.ktb.modie.repository;
 
+import java.util.Optional;
+
 import org.ktb.modie.domain.Meet;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
-import java.util.Optional;
 
 public interface MeetRepository extends JpaRepository<Meet, Long> {
     Optional<Meet> findByMeetId(Long meetId);
@@ -16,10 +16,11 @@ public interface MeetRepository extends JpaRepository<Meet, Long> {
     @Query("SELECT m FROM Meet m WHERE "
         + "(:meetType = '전체' OR m.meetType = :meetType) "
         + "AND ((:isCompleted = false AND m.completedAt IS NULL) "
-        + "OR (:isCompleted = true AND m.completedAt IS NOT NULL))")
+        + "OR (:isCompleted = true AND m.completedAt IS NOT NULL))"
+        + "AND m.deletedAt IS NULL")
     Page<Meet> findFilteredMeets(@Param("meetType") String meetType,
-                                 @Param("isCompleted") Boolean isCompleted,
-                                 Pageable pageable);
+        @Param("isCompleted") Boolean isCompleted,
+        Pageable pageable);
 
     // soft delete
     @Query("SELECT m FROM Meet m WHERE m.meetId = :meetId AND m.deletedAt IS NULL")
