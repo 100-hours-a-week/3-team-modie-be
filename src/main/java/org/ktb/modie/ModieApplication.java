5429a1677e4a8398a1c3bbba27ca909a251da26d
TAEHYEON <disney1026dlxogus@gmail.com>
2025-04-01 16:35:16 +0900

    fix: JPA 타임존 문제 해결

--- src/main/java/org/ktb/modie/ModieApplication.java
+++ src/main/java/org/ktb/modie/ModieApplication.java
@@ -1,7 +1,11 @@
 package org.ktb.modie;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ModieApplication {
@@ -9,6 +13,11 @@ public class ModieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ModieApplication.class, args);
     }
-    // hello
 
+    @PostConstruct
+    public void init() {
+        // JVM 전체 타임존을 KST로 고정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        System.out.println("✅ JVM 타임존 설정됨: " + TimeZone.getDefault().getID()); // 확인 로그
+    }
 }
